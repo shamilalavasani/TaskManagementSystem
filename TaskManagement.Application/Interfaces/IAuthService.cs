695a281c615dcbe8741f2e056b8f71c99e71e440
Shamila <shamilalavasani@gmail.com>
2026-04-11 13:13:40 +0400

    Add IAuthService interface for authentication operations

---
+++ TaskManagement.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿
+using TaskManagement.Application.DTOs.AuthDTOs;
+
+namespace TaskManagement.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request);
+    Task<AuthResponseDto> LoginAsync(LoginRequestDto request);
+}
