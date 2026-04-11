9207a27be00d0cc7b65b5712ad283e0d7a5d5308
Shamila <shamilalavasani@gmail.com>
2026-04-11 13:30:00 +0400

    Add IJwtTokenService interface for JWT generation

---
+++ TaskManagement.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskManagement.Application.Interfaces;
+
+public interface IJwtTokenService
+{
+    string GenerateToken(string userId, string email, IList<string> roles);//access token 
+    DateTime GetTokenExpiration();
+}
