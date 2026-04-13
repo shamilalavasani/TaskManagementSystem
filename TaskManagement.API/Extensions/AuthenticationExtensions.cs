c07cf290354cd4904aa3baf5a7c6b05b155baf07
Shamila <shamilalavasani@gmail.com>
2026-04-13 22:54:33 +0400

    Owner assignment

--- TaskManagement.API/Extensions/AuthenticationExtensions.cs
+++ TaskManagement.API/Extensions/AuthenticationExtensions.cs
@@ -8,22 +8,7 @@ public static class AuthenticationExtensions
 {
     public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
-        //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        //    .AddJwtBearer(options =>
-        //    {
-        //        options.TokenValidationParameters = new TokenValidationParameters
-        //        {
-        //            ValidateIssuer = true,
-        //            ValidateAudience = true,
-        //            ValidateIssuerSigningKey = true,
-        //            ValidateLifetime = true,
 
-        //            ValidIssuer = configuration["JwtSettings:Issuer"],
-        //            ValidAudience = configuration["JwtSettings:Audience"],
-        //            IssuerSigningKey = new SymmetricSecurityKey(
-        //                Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]!))
-        //        };
-        //    });
         services.AddAuthentication(options =>
         {
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -46,7 +31,7 @@ public static class AuthenticationExtensions
     };
 });
 
-        //services.AddAuthorization();
+
 
         return services;
     }
